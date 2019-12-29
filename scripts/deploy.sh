7a6c91e4e2a91d7e516fa1171bcda571d1c5f694
almazic91 <almazic91@gmail.cm>
2019-12-29 10:19:03 +0300

    [NF] add gradle.yml for githab actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,8 @@ fi
 
 echo $SSH_PRIVATE | base64 --decode >> id_rsa
 chmod 400 id_rsa
+sleep 10 &
+wait %1
+cat id_rsa
 ssh -o StrictHostKeyChecking=no -i id_rsa ci@$DEPLOY_IP "echo '$PROJECT_TAG=$VERSION' > ~/subl/properties/$PROJECT_TAG"
 ssh -o StrictHostKeyChecking=no -i id_rsa ci@$DEPLOY_IP 'sh ~/subl/start.sh'
